e47a8d9c24d97e4af71ce89b64f029171b31035f
Muhammad Furqan Sajjad <muhammadfurqansajjad@gmail.com>
2024-08-02 11:13:55 +0500

    Signup logic added

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
     name: "auth",
     initialState: {
-        logined: false,
+        logined: true,
         userData: {
             name: '',
             email: '',
